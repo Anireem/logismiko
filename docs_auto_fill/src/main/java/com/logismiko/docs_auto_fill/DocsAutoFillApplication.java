8880db23a92644c2ba35be76c3500741d1bd81fb
Anireem <gskorpofeo@gmail.com>
2024-03-12 14:38:54 +0500

    Initial commit.

---
+++ docs_auto_fill/src/main/java/com/logismiko/docs_auto_fill/DocsAutoFillApplication.java
@@ -0,0 +1,13 @@
+package com.logismiko.docs_auto_fill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocsAutoFillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocsAutoFillApplication.class, args);
+	}
+
+}
