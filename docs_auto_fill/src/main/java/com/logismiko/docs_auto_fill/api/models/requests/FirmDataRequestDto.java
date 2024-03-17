41de04280846411ceb37980b19fa6abac038dacc
Anireem <gskorpofeo@gmail.com>
2024-03-17 17:43:06 +0500

    Setup swagger docs.

--- docs_auto_fill/src/main/java/com/logismiko/docs_auto_fill/api/models/requests/FirmDataRequestDto.java
+++ docs_auto_fill/src/main/java/com/logismiko/docs_auto_fill/api/models/requests/FirmDataRequestDto.java
@@ -1,11 +1,18 @@
 package com.logismiko.docs_auto_fill.api.models.requests;
 
 import com.logismiko.docs_auto_fill.dao.entities.FirmDataEntity;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.io.Serializable;
 
 /**
  * A DTO for the {@link FirmDataEntity} entity
  */
-public record FirmDataRequestDto(Long id, String title, String description) implements Serializable {
+public record FirmDataRequestDto(
+    @Schema(description = "Дополнительные сведения", example = "Количество сотрудников")
+    String title,
+
+    @Schema(description = "Описание дополнительных сведений", example = "50 человек")
+    String description
+) implements Serializable {
 }
--- docs_auto_fill/src/main/java/com/logismiko/docs_auto_fill/utils/factories/FirmDataEntityFactory.java
+++ docs_auto_fill/src/main/java/com/logismiko/docs_auto_fill/utils/factories/FirmDataEntityFactory.java
@@ -4,7 +4,10 @@ import com.logismiko.docs_auto_fill.api.models.requests.FirmDataRequestDto;
 import com.logismiko.docs_auto_fill.dao.entities.FirmDataEntity;
 import com.logismiko.docs_auto_fill.utils.builders.FirmDataEntityBuilder;
 
-public class FirmDataEntityFactory {
+public final class FirmDataEntityFactory {
+    private FirmDataEntityFactory() {
+    }
+
     public static FirmDataEntity create(FirmDataRequestDto firmDataRequestDto) {
         return FirmDataEntityBuilder.aFirmDataEntity()
             .withTitle(firmDataRequestDto.title())
